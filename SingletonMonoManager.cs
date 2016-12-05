c30d916b79dc14c9094d68477c136a0dbc13c67b
Guitastr <253272489@qq.com>
2016-12-05 21:15:34 +0800

    继承MonoBehaviour的单例抽象类

---
+++ SingletonMonoManager.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public abstract class SingletonMonoManager<T> : MonoBehaviour where T : MonoBehaviour
+{
+    public static T Instance { get; private set; }
+
+    protected virtual void Awake()
+    {
+        Instance = this as T;
+    }
+}
