8f15e81073a485587e98eaa5030482371ced09a9
Guitastr <253272489@qq.com>
2016-12-05 21:14:22 +0800

    计算模型面数

---
+++ ModelNumberView.cs
@@ -0,0 +1,23 @@
+﻿   GUILayout.Label(" 总面数：" + trianglesCount);
+        if (GUILayout.Button("重新统计当前总面数"))
+        {
+            trianglesCount = 0;
+            TransformReset tr = new TransformReset(tranDog);
+            foreach (TransformInfo item in tr.TranInfoList)
+            {
+                if (item.transform.gameObject.activeInHierarchy)
+                {
+                    MeshFilter mf = item.transform.gameObject.GetComponent<MeshFilter>();
+                    if (mf != null)
+                    {
+                        //计算模型面数
+
+
+                        int count = mf.mesh.triangles.Length / 3;
+                        trianglesCount += count;
+                    }
+                }
+
+            }
+            Debug.Log(" 总面数：" + trianglesCount);
+        }
