4ca750bd644d0d201d8d7533c3170becd1afe3e6
Guitastr <253272489@qq.com>
2016-12-05 21:14:54 +0800

    资源加载类，Resources.LoadAll会遍历不同Resources，并找到符合的资源

---
+++ ResourcesLoadManager.cs
@@ -0,0 +1,34 @@
+﻿internal class ResourcesLoadManager : SingletonManager<ResourcesLoadManager>
+{
+    /// <summary>
+
+
+    /// 资源加载，加载文件下所有类型的资源
+
+
+    /// </summary>
+
+
+    /// <param name="path">Resources文件夹下的子目录</param>
+
+
+    /// <returns></returns>
+
+
+    public static IEnumerable<object> ResourcesLoadAll(string path)
+    {
+        object[] prefabs = Resources.LoadAll(path);
+
+        if (prefabs.Length == 0)
+        {
+            Debug.Log("资源不存在");
+            return null;
+        }
+        for (int i = 0; i < prefabs.Length; i++)
+        {
+            Debug.Log(prefabs[i] + "资源找到了");
+        }
+
+        return prefabs;
+    }
+}
