7950ba8fe1a3396298c40ae848505af89e5899e3
Guitastr <253272489@qq.com>
2016-12-05 21:13:58 +0800

    foreach迭代的内部逻辑代码

---
+++ ForeachView.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
+using UnityEngine;
+
+public class ForeachView : MonoBehaviour
+{
+    private void Start()
+    {
+        Person[] persons =
+        {
+            new Person{FirstName = "LL", LastName = "JJ"},
+            new Person{FirstName = "TT", LastName = "UU"}
+        };
+
+        foreach (Person person in persons)
+        {
+            Debug.Log(person);
+        }
+
+        Debug.Log("----------------------------------------------");
+
+        IEnumerator enumerator = persons.GetEnumerator();
+
+        while (enumerator.MoveNext())
+        {
+            Person p = enumerator.Current as Person;
+            Debug.Log(p);
+        }
+    }
+}
+
+public class Person : IComparable
+{
+    public string FirstName;
+    public string LastName;
+
+    public override string ToString()
+    {
+        return string.Format("FirstName: {0}, LastName: {1}", FirstName, LastName);
+    }
+}
