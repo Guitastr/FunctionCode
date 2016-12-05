cad338875329d2ebac50878c848ec2d959ba2851
Guitastr <253272489@qq.com>
2016-12-05 21:15:55 +0800

    开启新线程

---
+++ Thread_Test.cs
@@ -0,0 +1,14 @@
+﻿public class Thread_Test : MonoBehaviour
+{
+    private void Start()
+    {
+        Thread t = new Thread(new ThreadStart(Cal));
+        t.Start();
+    }
+
+    private void Cal()
+    {
+        print("新线程被开启");
+        GameObject.Find("Cube");
+    }
+}
