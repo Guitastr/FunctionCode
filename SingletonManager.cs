2561a5005181f608d90a980fac05626c7b8bf807
Guitastr <253272489@qq.com>
2016-12-05 21:15:27 +0800

    通用单例类

---
+++ SingletonManager.cs
@@ -0,0 +1,14 @@
+﻿public abstract class SingletonManager<T> where T : new()
+{
+    private static readonly T _instance;
+
+    static SingletonManager()
+    {
+        _instance = new T();
+    }
+
+    public static T Instance
+    {
+        get { return _instance; }
+    }
+}
