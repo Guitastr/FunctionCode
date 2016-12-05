5dceb1facaa70b387020e6a93f94ca1a2eed62be
Guitastr <253272489@qq.com>
2016-12-05 21:15:07 +0800

    一个物体围绕另一个物体旋转

---
+++ RotateAround.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+
+public class RotateAround : MonoBehaviour
+{
+    /// <summary>
+
+
+    ///     移动的速度
+
+
+    /// </summary>
+
+
+    public float Speed;
+
+    /// <summary>
+
+
+    ///     围绕运动的目标物体
+
+
+    /// </summary>
+
+
+    public Transform TargeTransform;
+
+    private void Update()
+    {
+        AroundRotateTransform(TargeTransform, transform, Speed);
+    }
+
+    /// <summary>
+
+
+    /// 围绕物体旋转
+
+
+    /// </summary>
+
+
+    /// <param name="targeTran">目标物体</param>
+
+
+    /// <param name="rotateTran">旋转物体</param>
+
+
+    /// <param name="speed">旋转速度</param>
+
+
+    public static void AroundRotateTransform(Transform targeTran, Transform rotateTran, float speed)
+    {
+        if (!targeTran) return;
+        rotateTran.LookAt(targeTran);
+        rotateTran.RotateAround(targeTran.position, Vector3.up, speed);
+    }
+}
